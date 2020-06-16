2b3e6d9f075ab7264ea4dbc9d03c4b1ba1f5a464
maiquelknechtel <maiquelknechtel@maiquelknechtels-MacBook-Pro.local>
2020-06-16 13:27:53 -0300

    first commit

---
+++ src/main/java/com/example/demo_s1/DemoS1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class DemoS1Application {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemoS1Application.class, args);
+//	}
+
+}
---
+++ src/main/java/io/spring/RealworldApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class RealworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldApplication.class, args);
+    }
+}
